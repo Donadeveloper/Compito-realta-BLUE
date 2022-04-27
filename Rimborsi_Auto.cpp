f190cdcf63cf177744329bc3fc28debe6db7c76a
Donadeveloper <lucadonadel005@gmail.com>
2022-04-27 10:54:46 +0200

    Added comments to Rimborsi_Auto.cpp

--- Rimborsi_Auto.cpp
+++ Rimborsi_Auto.cpp
@@ -8,16 +8,24 @@
 int main()
 {
     setlocale(LC_ALL, "");
+
+    // Apertura file csv contenente i dati
     std::fstream fileDati("DATIRIMBORSO.csv", std::fstream::in);
+
+    // Controllo che l'apertura sia andata a buon fine
     if (fileDati.fail()) {
         std::cout << "Lettura del file \"DATIRIMBORSO.csv\" fallita. Verificare che esso sia presente" << std::endl;
         exit(0);
     }
+
+    // Creazione vector principale contenente i dati e caricamento da file
     std::vector<datiRimborso> listaDati;
     caricamentoDati(listaDati, fileDati);
 
+    // Inizio output del programma
     mostramenu(listaDati, fileDati);
 
+    // Chiusura programma e file csv
     std::cout << "Chiusura programma. . .";
     fileDati.close();
     return 0;
